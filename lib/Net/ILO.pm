b7955c899bac013d21f208400fcb176825eb2611
nlewis <nick.lewis@gmail.com>
2010-07-26 13:12:43 -0700

    bumped version

--- lib/Net/ILO.pm
+++ lib/Net/ILO.pm
@@ -9,7 +9,7 @@ use English qw(-no_match_vars);
 use IO::Socket::SSL;
 use XML::Simple;
 
-our $VERSION = '0.41';
+our $VERSION = '0.42';
 
 
 my $METHOD_UNSUPPORTED = 'Method not supported by this iLO version';
