d472b592eecc8aa60df13e3bc103b755fa3bf2d8
Nick Lewis <nick.lewis@gmail.com>
2010-09-27 01:47:34 -0700

    as usual I forgot to bump the version number
    added 0.52 to Changes, credited Darko for _send() patch

--- lib/Net/ILO.pm
+++ lib/Net/ILO.pm
@@ -9,7 +9,7 @@ use English qw(-no_match_vars);
 use IO::Socket::SSL;
 use XML::Simple;
 
-our $VERSION = '0.51';
+our $VERSION = '0.52';
 
 
 my $METHOD_UNSUPPORTED = 'Method not supported by this iLO version';
